381c51533ffcaae487fc29529da4b9b8e632c9d6
Aman Varshney <av.amanvarshney11@gmail.com>
2023-12-07 13:43:43 +0530

    test

---
+++ src/components/name.tsx
@@ -0,0 +1,19 @@
+import { createSignal } from "solid-js";
+
+const NameTag = () => {
+
+  const [count, setCount] = createSignal(1);
+
+  return (
+    <div class="text-white text-2xl">
+      <div>
+        <h1 class="text-lg font-bold">Aman Varshney</h1>
+        <p class="">{count()}</p>
+        <button class="active:bg-pink-500" onClick={() => setCount(count() + 1)}>Increment</button>
+
+      </div>
+    </div>
+  );
+};
+
+export default NameTag;
