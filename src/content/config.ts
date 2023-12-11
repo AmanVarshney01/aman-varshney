0a4ca6cb10dd3176ce64e44be2a75445f25c9115
Aman Varshney <av.amanvarshney11@gmail.com>
2023-12-11 20:45:11 +0530

    Add getRelativeData function and update date format

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,8 @@ import { defineCollection, z } from "astro:content";
 const postsCollection = defineCollection({
   schema: z.object({
     title: z.string(),
-    date: z.date().transform((date) => date.toLocaleDateString('en-IN')),
+    // date: z.date().transform((date) => date.toLocaleDateString('en-IN')),
+    date: z.date(),
     image: z.string().optional(),
     featured: z.boolean(),
   }),
