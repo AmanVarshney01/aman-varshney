c9fd0fe6612618eec29fcc3a84b75acf27ea55df
Aman Varshney <av.amanvarshney11@gmail.com>
2023-12-12 22:45:23 +0530

    Update date format and remove unused import

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ const postsCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     // date: z.date().transform((date) => date.toLocaleDateString('en-IN')),
-    date: z.date(),
+    date: z.date().transform((date) => date.toString().slice(0, 15)),
     image: z.string().optional(),
     featured: z.boolean(),
   }),
