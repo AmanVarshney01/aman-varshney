7e24e56f1ad7838b4e3522700edee1be21c30d7c
Aman Varshney <av.amanvarshney11@gmail.com>
2023-12-19 19:33:02 +0530

    Add speedInsights to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,9 @@ export default defineConfig({
   integrations: [solidJs(), mdx(), tailwind(), sitemap()],
   output: "hybrid",
   adapter: vercel({
+    speedInsights: {
+      enabled: true,
+    },
     webAnalytics: {
       enabled: true,
     },
