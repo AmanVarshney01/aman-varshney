9bd3e1cf8d577576049699f79b98e394dfa2f488
Aman Varshney <av.amanvarshney11@gmail.com>
2024-03-12 00:25:43 +0530

    Update Astro dependencies and add image configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel/static";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://amanvarshney.tech/",
+  image: {
+    domains: ["img.shields.io"],
+    remotePatterns: [{ protocol: "https" }],
+  },
   integrations: [solidJs(), mdx(), tailwind(), sitemap()],
   output: "static",
   adapter: vercel({
