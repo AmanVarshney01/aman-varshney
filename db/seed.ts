1d586322a9369497dfbc198d301d904f9f83ba18
Aman Varshney <av.amanvarshney11@gmail.com>
2024-03-13 02:20:07 +0530

    Remove commented out code in seed.ts

--- db/seed.ts
+++ db/seed.ts
@@ -1,10 +1,4 @@
-import { db, Guestbook } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-  // await db.insert(Guestbook).values([
-  //   { name: "Astro", message: "Hello, world!" },
-  //   { name: "Astro", message: "Hello, world!" },
-  //   { name: "Astro", message: "Hello, world!" },
-  // ]);
 }
