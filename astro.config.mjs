a8a8623067a75fd6c2988cac42e10fbbfeb93d4c
Aman Varshney <av.amanvarshney11@gmail.com>
2024-06-23 17:04:05 +0530

    Add shiki transformers

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,36 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import solidJs from "@astrojs/solid-js";
-import vercel from "@astrojs/vercel/serverless";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import db from "@astrojs/db";
-// import {
-//   transformerNotationHighlight,
-//   transformerMetaHighlight,
-//   transformerNotationDiff,
-// } from "@shikijs/transformers";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import solidJs from '@astrojs/solid-js'
+import vercel from '@astrojs/vercel/serverless'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import db from '@astrojs/db'
+import {
+	transformerNotationDiff,
+	transformerNotationHighlight,
+	transformerNotationFocus,
+  transformerNotationWordHighlight
+} from '@shikijs/transformers'
 
 export default defineConfig({
-  site: "https://amanvarshney.tech/",
-  integrations: [solidJs(), mdx(), tailwind(), sitemap(), db()],
-  output: "hybrid",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    imageService: true,
-  }),
-  markdown: {
-    shikiConfig: {
-      theme: "material-theme-darker",
-    },
-  },
-});
+	site: 'https://amanvarshney.tech/',
+	integrations: [solidJs(), mdx(), tailwind(), sitemap(), db()],
+	output: 'hybrid',
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true
+		},
+		imageService: true
+	}),
+	markdown: {
+		shikiConfig: {
+			theme: 'vitesse-dark',
+			transformers: [
+				transformerNotationDiff(),
+				transformerNotationHighlight(),
+				transformerNotationFocus(),
+        transformerNotationWordHighlight()
+			]
+		}
+	}
+})
