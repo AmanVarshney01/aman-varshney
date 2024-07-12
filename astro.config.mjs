c64a040c18f13f1c0178d9dae1b2d9c72f6d7403
Aman Varshney <av.amanvarshney11@gmail.com>
2024-07-13 02:47:11 +0530

    Add gradient background

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,10 +26,6 @@ export default defineConfig({
 	markdown: {
 		shikiConfig: {
 			theme: 'vitesse-dark',
-			// themes: {
-			// 	light: 'vitesse-light',
-			// 	dark: 'vitesse-dark'
-			// },
 			transformers: [
 				transformerNotationDiff(),
 				transformerNotationHighlight(),
