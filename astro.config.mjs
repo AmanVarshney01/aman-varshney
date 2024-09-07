26e371c3e8226605dc9df4120a24f3f548e2fc68
Aman Varshney <av.amanvarshney11@gmail.com>
2024-09-07 19:12:09 +0530

    add astro-icon package and improve UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,29 +7,25 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import expressiveCode from 'astro-expressive-code'
 import { defineConfig } from 'astro/config'
 
+import icon from 'astro-icon';
+
 export default defineConfig({
-	site: 'https://amanvarshney.tech/',
-	integrations: [
-		expressiveCode({
-			plugins: [pluginLineNumbers()],
-			themes: ['vitesse-black', 'vitesse-light'],
-			useDarkModeMediaQuery: true,
-			themeCssSelector: () => "[data-theme='lofi']"
-		}),
-		mdx(),
-		tailwind(),
-		sitemap(),
-		db()
-	],
-	output: 'hybrid',
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true
-		},
-		imageService: true
-	}),
-	experimental: {
-		serverIslands: true
-	},
-	prefetch: true
+    site: 'https://amanvarshney.tech/',
+    integrations: [expressiveCode({
+        plugins: [pluginLineNumbers()],
+        themes: ['vitesse-black', 'vitesse-light'],
+        useDarkModeMediaQuery: true,
+        themeCssSelector: () => "[data-theme='lofi']"
+		}), mdx(), tailwind(), sitemap(), db(), icon()],
+    output: 'hybrid',
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true
+        },
+        imageService: true
+    }),
+    experimental: {
+        serverIslands: true
+    },
+    prefetch: true
 })
