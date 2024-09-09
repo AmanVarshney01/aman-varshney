457ba1b6815e84fc74a6daab370f199c1b75bba5
Aman Varshney <av.amanvarshney11@gmail.com>
2024-09-09 23:50:43 +0530

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,29 +5,35 @@ import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import expressiveCode from 'astro-expressive-code'
+import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
-import icon from 'astro-icon';
-
 export default defineConfig({
-    site: 'https://amanvarshney.tech/',
-    integrations: [expressiveCode({
-        plugins: [pluginLineNumbers()],
-        themes: ['vitesse-black', 'vitesse-light'],
-        useDarkModeMediaQuery: true,
-        themeCssSelector: () => "[data-theme='lofi']"
-		}), mdx(), tailwind(), sitemap(), db(), icon({
-            iconDir: "src/assets/icons",
-        })],
-    output: 'hybrid',
-    adapter: vercel({
-        webAnalytics: {
-            enabled: true
-        },
-        imageService: true
-    }),
-    experimental: {
-        serverIslands: true
-    },
-    prefetch: true
+	site: 'https://amanvarshney.tech/',
+	integrations: [
+		expressiveCode({
+			plugins: [pluginLineNumbers()],
+			themes: ['vitesse-black', 'vitesse-light'],
+			useDarkModeMediaQuery: true,
+			themeCssSelector: () => "[data-theme='lofi']"
+		}),
+		mdx(),
+		tailwind(),
+		sitemap(),
+		db(),
+		icon({
+			iconDir: 'src/assets/icons'
+		})
+	],
+	output: 'hybrid',
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true
+		},
+		imageService: true
+	}),
+	experimental: {
+		serverIslands: true
+	},
+	prefetch: true
 })
