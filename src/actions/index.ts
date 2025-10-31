6b5f7ae582e30f9999b9c78f1218283c0416335a
Aman Varshney <amanvarshney.work@gmail.com>
2025-10-31 21:46:46 +0530

    add min and max in guestbook inputs

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -6,8 +6,14 @@ export const server = {
 	guestbook: defineAction({
 		accept: 'form',
 		input: z.object({
-			name: z.string(),
-			message: z.string()
+			name: z
+				.string()
+				.min(2, { message: 'Name must be at least 2 characters.' })
+				.max(50, { message: 'Name must be at most 50 characters.' }),
+			message: z
+				.string()
+				.min(2, { message: 'Message must be at least 2 characters.' })
+				.max(200, { message: 'Message must be at most 200 characters.' })
 		}),
 		handler: async ({ name, message }) => {
 			await db.insert(Guestbook).values({ name, message })
